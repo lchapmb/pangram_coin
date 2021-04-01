3f49ca5aa7ecbe4d7ed39ea5edb300e783a5ebfd
lchapmb <lchapmb@googlemail.com>
2021-04-01 16:14:24 +0100

    adds nonce values and difficulty for proof of work validation

--- pangram.js
+++ pangram.js
@@ -7,35 +7,69 @@ class CryptoBlock {
     this.data = data;
     this.precedingHash = precedingHash;
     this.hash = this.computeHash();
+    this.nonce = 0;
   }
   computeHash() {
     return SHA256(
       this.index +
         this.precedingHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
+    }
   }
 }
 
 class CryptoBlockchain {
   constructor() {
     this.blockchain = [this.startGenesisBlock()];
+    // changing the number below will affect the speed of generating coins
+    this.difficulty = 4;
   }
+
   startGenesisBlock() {
     return new CryptoBlock(0, '01/01/2021', 'Initial Block in the Chain', '0');
   }
+
   obtainLatestBlock() {
     return this.blockchain[this.blockchain.length - 1];
   }
+
   addNewBlock(newBlock) {
     newBlock.precedingHash = this.obtainLatestBlock().hash;
-    newBlock.hash = newBlock.computeHash();
+    // newBlock.hash = newBlock.computeHash();
+    newBlock.proofOfWork(this.difficulty);
     this.blockchain.push(newBlock);
+  }
+
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currentBlock = this.blockchain[i];
+      const precedingBlock = this.blockchain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+      if (currentBlock.precedingHash !== precedingBlock.hash) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
 let pangramCoin = new CryptoBlockchain();
+
+console.log('pangramCoin mining in progess...');
 pangramCoin.addNewBlock(
   new CryptoBlock(1, '01/06/2021', {
     sender: 'Iris Ljesnjanin',
