eb1cc456c8dbb37c6dc4c2d8a3a30ad76a69e7f9
lchapmb <lchapmb@googlemail.com>
2021-04-01 15:57:21 +0100

    adds class of CryptoBlock and CryptoBlockchain

--- pangram.js
+++ pangram.js
@@ -1,4 +1,5 @@
 const SHA256 = require('crypto-js/sha256');
+
 class CryptoBlock {
   constructor(index, timestamp, data, precedingHash = ' ') {
     this.index = index;
@@ -16,3 +17,37 @@ class CryptoBlock {
     ).toString();
   }
 }
+
+class CryptoBlockchain {
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+  }
+  startGenesisBlock() {
+    return new CryptoBlock(0, '01/01/2021', 'Initial Block in the Chain', '0');
+  }
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock) {
+    newBlock.precedingHash = this.obtainLatestBlock().hash;
+    newBlock.hash = newBlock.computeHash();
+    this.blockchain.push(newBlock);
+  }
+}
+
+let pangramCoin = new CryptoBlockchain();
+pangramCoin.addNewBlock(
+  new CryptoBlock(1, '01/06/2021', {
+    sender: 'Iris Ljesnjanin',
+    recipient: 'Cosima Mielke',
+    quantity: 50,
+  })
+);
+pangramCoin.addNewBlock(
+  new CryptoBlock(2, '01/07/2021', {
+    sender: 'Vitaly Friedman',
+    recipient: 'Ricardo Gimenes',
+    quantity: 100,
+  })
+);
+console.log(JSON.stringify(pangramCoin, null, 4));
